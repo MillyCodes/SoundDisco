7c4c80ed7ab598a2a71e4543842d1f780cd47fd6
Milly R <millz118@gmail.com>
2018-09-26 17:18:50 -0400

    index samples randomized

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,9 @@ class HomeController < ApplicationController
   def index
       @lastfm = Lastfm.new(ENV["LASTFM_KEY"], ENV["LASTFM_SECRET"])
       
-      @recentartists = @lastfm.chart.get_top_artists[17..19]
-      @topartists = @lastfm.chart.get_top_artists[0..5]
+      @recentartists = @lastfm.chart.get_top_artists.sample(3)
+
+      @topartists = @lastfm.chart.get_top_artists.sample(6)
       render :layout => 'index'
     end
 end
